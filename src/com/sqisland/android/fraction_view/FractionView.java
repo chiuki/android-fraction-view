427ef7b23891c00ae088a3ced06fa75ab99fbb46
Chiu-Ki Chan <chiuki@gmail.com>
2013-06-06 22:37:30 -0700

    Draw circle

--- src/com/sqisland/android/fraction_view/FractionView.java
+++ src/com/sqisland/android/fraction_view/FractionView.java
@@ -1,11 +1,15 @@
 package com.sqisland.android.fraction_view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class FractionView extends View {
+  private Paint mCirclePaint;
+
   public FractionView(Context context) {
     super(context);
     init();
@@ -22,6 +26,24 @@ public class FractionView extends View {
   }
 
   private void init() {
+    // Avoid allocating new memory in onDraw by initializing mCirclePaint
+    // in the constructor.
+    mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mCirclePaint.setColor(Color.CYAN);
+    mCirclePaint.setStyle(Paint.Style.FILL);
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
     setBackgroundColor(Color.LTGRAY);
+
+    int width = getWidth() - getPaddingLeft() - getPaddingRight();
+    int height = getHeight() - getPaddingTop() - getPaddingBottom();
+    int size = Math.min(width, height);
+    int cx = width / 2 + getPaddingLeft();
+    int cy = height / 2 + getPaddingTop();
+    int radius = size / 2;
+
+    canvas.drawCircle(cx, cy, radius, mCirclePaint);
   }
 }
